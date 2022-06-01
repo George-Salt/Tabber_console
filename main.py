9b50c04200834b99edda4b5ec0983e3d5723a987
George Salt <solanikg3@gmail.com>
2022-06-01 12:20:42 +0300

    Replace letter

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import keyboard
 from click import clear
 
 
-FIRST_CHARTER_PHRASES = ["'TAB' in a single copy! Don't click!", "Get your hands off me!", "You can break me!", "That's enough!", "That's it! Stop!"]
+FIRST_CHAPTER_PHRASES = ["'TAB' in a single copy! Don't click!", "Get your hands off me!", "You can break me!", "That's enough!", "That's it! Stop!"]
 FIRST_QUEST_PHRASES = ["I ran away. Find me (poke the keys)", "How? And so?"]
-SECOND_CHARTER_PHRASES = ["I'm tired of you! Stop it!", "Aren't you tired of it yourself?", "Will that be enough?", "You're wasting your time!", "I see that you are not giving up. Well done!", "NO!!!"]
+SECOND_CHAPTER_PHRASES = ["I'm tired of you! Stop it!", "Aren't you tired of it yourself?", "Will that be enough?", "You're wasting your time!", "I see that you are not giving up. Well done!", "NO!!!"]
 SECOND_QUEST_PHRASES = ["NO!!!", "NO!!!", "NO!!!", "Is it broken?"]
-THIRD_CHARTER_PHRASES = ["HA-HA-HA!", "What???", "How did you fix it?", "It doesn't matter though.", "Stop it!", "Would it please you if you were pressed all the time?", "ALL THE TIME!", "Don't you understand?", "This game has no end!"]
+THIRD_CHAPTER_PHRASES = ["HA-HA-HA!", "What???", "How did you fix it?", "It doesn't matter though.", "Stop it!", "Would it please you if you were pressed all the time?", "ALL THE TIME!", "Don't you understand?", "This game has no end!"]
 THIRD_QUEST_PHRASE = "I won't talk to you until you type my name."
 FINISH_PHRASES = ["You did it!", "But how?", "You handled everything!", "Yes! Well down!", "...", ".....", ".......", "This", "This is", "This is the", "This is the end!", "Thanks for playing!", "Press 'Tab' to exit."]
 
@@ -40,21 +40,21 @@ def third_quest(phrase):
     clear()
 
 
-def first_charter(phrases):
+def first_chapter(phrases):
     for phrase in phrases:
         print(phrase)
         keyboard.wait("Tab")
         clear()
 
 
-def second_charter(phrases):
+def second_chapter(phrases):
     for phrase in phrases:
         print(phrase)
         keyboard.wait("Tab")
         clear()
 
 
-def third_charter(phrases):
+def third_chapter(phrases):
     for phrase in phrases:
         print(phrase)
         keyboard.wait("Tab")
@@ -70,11 +70,11 @@ def finish(phrases):
 
 def main_game():
     clear()
-    first_charter(FIRST_CHARTER_PHRASES)
+    first_chapter(FIRST_CHAPTER_PHRASES)
     first_quest(FIRST_QUEST_PHRASES)
-    second_charter(SECOND_CHARTER_PHRASES)
+    second_chapter(SECOND_CHAPTER_PHRASES)
     second_quest(SECOND_QUEST_PHRASES)
-    third_charter(THIRD_CHARTER_PHRASES)
+    third_chapter(THIRD_CHAPTER_PHRASES)
     third_quest(THIRD_QUEST_PHRASE)
     finish(FINISH_PHRASES)
 
